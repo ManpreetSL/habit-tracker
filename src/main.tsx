1a419d6dde1f55026bac34460d081a5cca63738f
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2022-12-05 23:39:20 +0000

    feat: add error page to router

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import ErrorPage from './views/ErrorPage';
 import About from './views/About';
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
   },
   {
     path: '/about/',
     element: <About />,
+    errorElement: <ErrorPage />,
   },
 ]);
 

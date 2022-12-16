87bde8d839b1cc0f56b64eddaccc493b5734148a
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2022-12-16 20:15:44 +0000

    style: change Home component to be exported separated to its definition

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
 
-export default function Home() {
+const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -43,3 +43,5 @@ export default function Home() {
     </div>
   );
 }
+
+export default Home;

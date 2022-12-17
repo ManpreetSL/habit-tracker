80ba4a087fc8dc2dba01a4b47ff9d6806b0bcc0b
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2022-12-17 15:21:11 +0000

    feat: add habit details to view habits screen

--- components/ViewHabits.tsx
+++ components/ViewHabits.tsx
@@ -1,20 +1,52 @@
 import { css } from '@emotion/react';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import Habit from './Habit';
+import { Habit as HabitType } from '../data-types';
 
-type Props = {};
+const fetchData = async () => {
+  const data = await fetch('../data/habits.json');
+  const json = await data.json();
+  return json;
+};
 
-const ViewHabits = (props: Props) => (
-  <div css={containerStyle}>
-    <Image src='/logo.svg' alt='.SHIFT logo' fill />
-  </div>
-);
+const ViewHabits = () => {
+  const [habitsData, setHabitsData] = useState<HabitType[]>([]);
+  useEffect(() => {
+    const fetch = async () => {
+      const data = await fetchData();
+      setHabitsData(data);
+    };
+    fetch();
+  }, []);
+
+  return (
+    <div css={containerStyle}>
+      <>
+        <div css={logoContainerStyle}></div>
+        <Image css={logoStyle} src='/logo.svg' alt='.SHIFT logo' fill />
+        {habitsData.map((habit) => (
+          <Habit key={habit.id} habit={habit} />
+        ))}
+      </>
+    </div>
+  );
+};
 
 const containerStyle = css({
   position: 'relative',
   width: '100vw',
   maxWidth: '100%',
-  height: '20vh',
+  height: '100vh',
   backgroundColor: '#111'
+});
+
+const logoContainerStyle = css({
+  height: '20vh'
+});
+
+const logoStyle = css({
+  maxHeight: '16vh'
 });
 
 export default ViewHabits;

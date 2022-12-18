3a51a9385976ff0ee8a20cc2b3c0287e2438147f
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2022-12-18 14:26:28 +0000

    refactor: change Next global CSS to Emotion global CSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,36 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { Global, css } from '@emotion/react';
+
+const globalStyles = css({
+  '*': {
+    boxSizing: 'border-box'
+  },
+  '@media (prefers-color-scheme: dark)': {
+    body: {
+      color: '#fff',
+      backgroundColor: '#000'
+    },
+    html: {
+      colorScheme: 'dark'
+    }
+  },
+  body: {
+    padding: 0,
+    margin: 0,
+    fontFamily:
+      '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif'
+  },
+  a: {
+    color: 'inherit',
+    textDecoration: 'none'
+  }
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global styles={globalStyles} />
+      <Component {...pageProps} />
+    </>
+  );
 }

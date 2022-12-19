3a5cc00654f1097e739e21b040afd2835ff8c008
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2022-12-19 22:01:30 +0000

    feat: add types for extra info like goals, habit entries and history
    Also expanded the habit type to support habits with binary and non-binary completion types. This structure will make it easier to work with the habits data.

--- data-types.ts
+++ data-types.ts
@@ -1,7 +1,46 @@
-export type Habit = {
+// If binary, then yes/no completion, if not, then it's numerical completion
+export type HabitType = 'binary' | 'non-binary';
+
+// Action that leads to fulfilment in your life
+export type CoreHabit = {
   id: string;
   name: string;
+  description?: string;
   frequency: string; // How often? Daily/Weekly/Twice weekly habit?
-  streak: number;
+  intensity?: number;
+  impact?: number; // Life-wise and feelings-wise
+};
+
+export type Binary = CoreHabit & {
+  type: 'binary';
+};
+
+export type NonBinary = CoreHabit & {
+  type: 'non-binary';
+  target: number; // How many times do we want to do this habit in the day?
+};
+
+export type Habit = Binary | NonBinary;
+
+// Single recording of a habit
+// Captures the completion, effort, metadata.
+export type Entry = {
   complete: boolean;
+  completionDate: Date;
+  effort?: number;
+  notes?: string;
+};
+
+// The full history of a habit
+export type HabitHistory = {
+  habit: Habit;
+  entries: Entry[];
+  streak: number;
+};
+
+// An overarching objective, formed by a collection of habits
+export type Goal = {
+  name: string;
+  description?: string;
+  habits: HabitHistory[];
 };

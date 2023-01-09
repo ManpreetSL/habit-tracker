bcc0b67459bbf722542bd8697b7521dbaaebb332
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2023-01-09 07:47:44 +0000

    feat: set up i18n localisation and add translations to the About page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,24 @@
-const About = () => <h1>About</h1>;
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+export async function getStaticProps({ locale = 'en' }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common', 'about']))
+      // Will be passed to the page component as props
+    }
+  };
+}
+
+const About = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div>
+      <h1>{t('common:app.title')}</h1>
+      <h2>{t('about:about')}</h2>
+    </div>
+  );
+};
 
 export default About;

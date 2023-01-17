f86287410c7bd1139e4c540e2dfde89b624eab03
Manpreet <ManpreetSL@users.noreply.github.com>
2023-01-17 18:41:55 +0000

    Revert "feat: set up basic layout for adding a new habit"
    This reverts commit 747dff6bc193bb36f2610f1619a7a269d0e7b04e.

--- components/ViewHabits.tsx
+++ components/ViewHabits.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from 'react';
 import Habit from './Habit';
 import { GoalWithHabitHistory } from '../src/types/habits';
 import { fetchData } from '../src/api/habits';
-import Link from '../src/components/Link';
 
 const styles = {
   container: css({
@@ -14,22 +13,11 @@ const styles = {
     maxHeight: '100vh',
     backgroundColor: '#111'
   }),
-  header: css({
-    display: 'flex',
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-    margin: '20px'
-  }),
   logoContainer: css({
-    height: '20vh',
-    width: '100vw'
+    height: '20vh'
   }),
   logo: css({
-    maxHeight: '16vh',
-    maxWidth: '100vw'
-  }),
-  addHabitButton: css({
-    position: 'relative'
+    maxHeight: '16vh'
   })
 };
 
@@ -42,15 +30,9 @@ const ViewHabits = () => {
 
   return (
     <div css={styles.container}>
-      <header css={styles.header}>
-        <div css={styles.logoContainer}>
-          <Image css={styles.logo} src='/logo.svg' alt='.SHIFT logo' fill />
-        </div>
-
-        <div css={styles.addHabitButton}>
-          <Link href={`/add-habit`}>Addddddd</Link>
-        </div>
-      </header>
+      <div css={styles.logoContainer}>
+        <Image css={styles.logo} src='/logo.svg' alt='.SHIFT logo' fill />
+      </div>
 
       {habitsData.map(({ habits }) =>
         habits.map(({ entries, streak, ...habit }) => (

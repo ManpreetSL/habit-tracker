389e0b3507386c5986320c3889123c304b02ae48
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2023-01-17 18:40:54 +0000

    fix: fix existing ESLint errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,34 +3,34 @@ import { Global, css } from '@emotion/react';
 
 const globalStyles = css({
   '*': {
-    boxSizing: 'border-box'
+    boxSizing: 'border-box',
   },
   '@media (prefers-color-scheme: dark)': {
     body: {
       color: '#fff',
-      backgroundColor: '#000'
+      backgroundColor: '#000',
     },
     html: {
-      colorScheme: 'dark'
-    }
+      colorScheme: 'dark',
+    },
   },
   body: {
     padding: 0,
     margin: 0,
     fontFamily:
-      '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif'
+      '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
   },
   a: {
     color: 'inherit',
-    textDecoration: 'none'
-  }
+    textDecoration: 'none',
+  },
 });
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Global styles={globalStyles} />
-      <Component {...pageProps} />
-    </>
-  );
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Global styles={globalStyles} />
+    <Component {...pageProps} />
+  </>
+);
+
+export default App;

e8ee636ab8c2a9d596eaaa3106aa961b1f385a34
Manpreet Singh Lalria <manpreet.singh.lalria@gmail.com>
2023-01-31 00:19:23 +0000

    feat: add local storage functionality so data can be saved and retrieved
    feat: make fetchData() fetch data from localStorage instead of a file
    
    refactor: move JSON habits paring to new parseJsonHabits() function
    
    refactor: create saveHabits() to break down saveDefaultData()
    
    This allows us to use saveHabits() functionality separately when we want to save non-default data.
    
    refactor: move local storage methods from separate file to JSON factory
    
    fix: turn Promise rejection strings into Error objects
    
    Fixes ESLint prefer-promise-reject-errors errors.
    
    fix: change HTML button element to Button component with button type
    
    Fixes an ESLint react/button-has-type error.

--- src/api/habits/adapters/json.ts
+++ src/api/habits/adapters/json.ts
@@ -9,24 +9,48 @@ const jsonHabitServiceFactory = (): HabitService => {
   const addHabit = (): Promise<string> =>
     Promise.resolve('adding habit in ze factorie');
 
-  const getHabits = (): Promise<GoalWithHabitHistory[]> =>
-    fetch('../data/habits.json')
-      .then((res) => res.json() as Promise<Response>)
-      .then((json) =>
-        json.map(({ habits, ...rest }) => ({
-          ...rest,
-          habits: habits.map(({ entries, frequency, ...restHabits }) => ({
-            ...restHabits,
-            frequency: frequency as Frequency,
-            entries: entries.map(({ completionDate, ...restEntries }) => ({
-              ...restEntries,
-              completionDate: new Date(completionDate),
-            })),
-          })),
-        }))
-      );
+  const parseJsonHabits = (json: string) => {
+    const jsonHabits = JSON.parse(json) as unknown as Response;
 
-  return { addHabit, getHabits };
+    return jsonHabits.map(({ habits, ...rest }) => ({
+      ...rest,
+      habits: habits.map(({ entries, frequency, ...restHabits }) => ({
+        ...restHabits,
+        frequency: frequency as Frequency,
+        entries: entries.map(({ completionDate, ...restEntries }) => ({
+          ...restEntries,
+          completionDate: new Date(completionDate),
+        })),
+      })),
+    }));
+  };
+
+  const getHabits = (): Promise<GoalWithHabitHistory[]> => {
+    const localHabits = localStorage.getItem('habits');
+    console.log('localHabits', localHabits);
+    if (!localHabits) return Promise.reject(new Error('No habits found'));
+
+    try {
+      return Promise.resolve(parseJsonHabits(localHabits));
+    } catch (error) {
+      console.error('Error fetching habits');
+      return Promise.reject(new Error('Failed to fetch habits'));
+    }
+  };
+
+  const saveHabits = (habits: GoalWithHabitHistory[]) => {
+    localStorage.setItem('habits', JSON.stringify(habits));
+  };
+
+  // Save a default set of habits to local storage
+  const saveDefaultData = () => {
+    console.log('saving default data');
+    fetch('../data/habits.json')
+      .then((res) => res.json())
+      .then((json) => saveHabits(parseJsonHabits(JSON.stringify(json))));
+  };
+
+  return { addHabit, getHabits, saveDefaultData };
 };
 
 export default jsonHabitServiceFactory;

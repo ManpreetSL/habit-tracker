73aebaf4467c523a136934c5d5c595c5e628ff3c
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2023-02-19 23:38:13 +0000

    refactor: simplify Button so styles can be passed through a prop

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,32 +3,18 @@ import { css, SerializedStyles } from '@emotion/react';
 const styles = {
   button: css({
     padding: '0.5em 2em',
+    color: '#000',
+    backgroundColor: '#91E220',
   }),
 };
 
 type ButtonProps = {
-  color?: string;
-  backgroundColor?: string;
   children: React.ReactNode;
   stylesProp?: SerializedStyles;
 } & React.ComponentProps<'button'>;
 
-const Button = ({
-  color = '#000',
-  backgroundColor = '#91E220',
-  stylesProp,
-  children,
-  ...rest
-}: ButtonProps) => (
-  <button
-    css={[
-      styles.button,
-      { color: `${color}`, backgroundColor: `${backgroundColor}` },
-      { ...stylesProp },
-    ]}
-    type='button'
-    {...rest}
-  >
+const Button = ({ children, stylesProp, ...rest }: ButtonProps) => (
+  <button css={[styles.button, { ...stylesProp }]} type='button' {...rest}>
     {children}
   </button>
 );

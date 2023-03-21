5d07702d10b0777c719d003ba2a2e510c0d12c90
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2023-03-21 07:00:01 +0000

    test: add email already in use test for sign up page

--- pages/sign-up.test.tsx
+++ pages/sign-up.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { signUp } from '../src/services/auth/manage-users';
 import SignUp from './sign-up.page';
 
 jest.mock('firebase/auth', () => ({
@@ -15,6 +16,12 @@ jest.mock('next/router', () => ({
   useRouter: jest.fn().mockReturnValue({}),
 }));
 
+jest.mock('../src/services/auth/manage-users', () => ({
+  signUp: jest
+    .fn()
+    .mockRejectedValue('Firebase: Error (auth/email-already-in-use).'),
+}));
+
 describe('SignUp()', () => {
   it('should render without crashing', () => {
     render(<SignUp />);
@@ -22,11 +29,35 @@ describe('SignUp()', () => {
     expect(screen.getByRole('heading')).toHaveTextContent('auth:signUp');
   });
 
-  it.todo(
-    'should show an error message if the email address already has an existing account associated with it'
-  );
+  it('should show an error message if the email address already has an existing account associated with it', async () => {
+    const user = userEvent.setup();
+    render(<SignUp />);
 
-  it.todo('should show an error message if an invalid password is entered');
+    const usernameInput = screen.getByLabelText('auth:email:', {
+      selector: 'input',
+    });
+    const passwordInput = screen.getByLabelText('auth:password:', {
+      selector: 'input',
+    });
+
+    const submitButton = screen.getByRole('button', { name: 'auth:signUp' });
+
+    await user.type(usernameInput, 'Laxus@FairyTail.guild');
+    await user.type(passwordInput, 'Jellaaaal1212');
+    await user.click(submitButton);
+
+    expect(signUp).toHaveBeenCalled();
+    expect(signUp).rejects.toEqual(
+      'Firebase: Error (auth/email-already-in-use).'
+    );
+    setTimeout(() => {
+      expect(screen.queryByText(/auth:errors.emailInUse/i)).not.toBeNull();
+    }, 1000);
+  });
+
+  it.todo(
+    'should show an error message if an password with <6 characters is entered'
+  );
 
   it.todo('should successfully log in an existing user');
 

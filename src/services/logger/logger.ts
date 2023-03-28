42e0b0d22b40068595d9bec13daabcda0c9e49fe
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2023-03-28 01:20:08 +0100

    refactor: simplify logger service

--- src/services/logger/logger.ts
+++ src/services/logger/logger.ts
@@ -3,32 +3,12 @@ import log from 'loglevel';
 const defaultLevel = process.env.NODE_ENV === 'production' ? 'info' : 'trace';
 log.setDefaultLevel(defaultLevel);
 
-const trace = (...msg: any[]) => {
-  log.trace(...msg);
-};
-
-const debug = (...msg: any[]) => {
-  log.debug(...msg);
-};
-
-const info = (...msg: any[]) => {
-  log.info(...msg);
-};
-
-const warn = (...msg: any[]) => {
-  log.warn(...msg);
-};
-
-const error = (...msg: any[]) => {
-  log.error(...msg);
-};
-
 const logger = {
-  trace,
-  debug,
-  info,
-  warn,
-  error,
+  trace: log.trace,
+  debug: log.debug,
+  info: log.info,
+  warn: log.warn,
+  error: log.error,
 };
 
 export default logger;

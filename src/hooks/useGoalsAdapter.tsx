9c4806f9e37e9f0331a58f63bfff5fea57dbbc3b
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2023-05-25 23:13:55 +0100

    refactor: change goals adapter to only use the REST habit service

--- src/hooks/useGoalsAdapter.tsx
+++ src/hooks/useGoalsAdapter.tsx
@@ -1,17 +1,5 @@
-import useUser from '../services/auth/useUser';
-import { jsonHabitService, restHabitService } from '../api/habits';
+import { restHabitService } from '../api/habits';
 
-import logger from '../services/logger';
-
-const useGoalsAdapter = () => {
-  const { user } = useUser();
-
-  // Return JSON local storage methods
-  if (!user) {
-    return jsonHabitService;
-  }
-
-  return restHabitService;
-};
+const useGoalsAdapter = () => restHabitService;
 
 export default useGoalsAdapter;

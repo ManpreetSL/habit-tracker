cfe27d98a52a3ca992fb42a354139ebd7533f06f
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2023-11-01 06:31:04 +0000

    feat: set up controller for goals

---
+++ pages/api/goals/controller.ts
@@ -0,0 +1,55 @@
+import { transformGoals } from '../goal-utils';
+import prisma from '../../../prisma/prisma-db';
+import { GoalWithHabitsAndEntries } from '../../../prisma/types';
+import logger from '../../../src/services/logger';
+
+type GetGoalsParams = {
+  userId: string;
+  fromDate: Date;
+  toDate: Date;
+};
+
+export const getGoals = async ({
+  userId,
+  fromDate,
+  toDate,
+}: GetGoalsParams) => {
+  let goals: GoalWithHabitsAndEntries[] = [];
+
+  if (!userId) {
+    throw new Error(`Invalid user ID ${userId}`);
+  }
+
+  logger.debug(`getGoals for user ${userId} between ${fromDate} and ${toDate}`);
+
+  goals = await prisma.goal.findMany({
+    where: {
+      userId,
+    },
+    include: {
+      habits: {
+        include: {
+          entries: true,
+        },
+      },
+    },
+  });
+
+  logger.debug('getGoals goals', goals);
+
+  const transformedGoals = transformGoals(goals);
+
+  // logger.info(
+  //   'controller habit quantity',
+  //   transformedGoals[0]?.habits[0].targetQuantity
+  // );
+
+  // logger.info(
+  //   'controller type',
+  //   transformedGoals[0]?.habits[0]?.entries[0]?.completionDate instanceof Date
+  // );
+
+  return transformedGoals;
+};
+
+export default {};

3bb6afb2c380a24af1a5b65e6a66e3ff47925fc1
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2023-11-30 07:06:38 +0000

    feat: add Prisma deletion for DELETE /api/entries/[entryId] route

--- pages/api/entries/[entryId].api.ts
+++ pages/api/entries/[entryId].api.ts
@@ -1,19 +1,23 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import logger from '../../../src/services/logger';
+import prisma from '../../../prisma/prisma-db';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  logger.debug(req.method, '@/api/entries');
   if (req.method === 'DELETE') {
     const { entryId } = req.query;
 
     if (!entryId) return res.status(405).end();
     if (typeof entryId !== 'string') return res.status(405).end();
 
-    // Delete entry <id>
-    return res.status(200);
+    const response = await prisma.entry.delete({
+      where: {
+        id: entryId,
+      },
+    });
+
+    return res.status(200).json(response);
   }
 
   return res.status(500);

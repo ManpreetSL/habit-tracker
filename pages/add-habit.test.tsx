b26adaa6cfddc4f2d728505570d08bafdebf0665
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2024-03-10 17:05:51 +0000

    test: add test cases that have yet to be implemented

--- pages/add-habit.test.tsx
+++ pages/add-habit.test.tsx
@@ -13,4 +13,6 @@ describe('AddHabit', () => {
       screen.queryByRole('heading', { name: 'add-habit:title' })
     ).toBeInTheDocument();
   });
+
+  it.todo('should allow a user to create a new habit');
 });
--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -33,4 +33,15 @@ describe('<Home />', () => {
     // Assert
     expect(await screen.findByText('app:links.about')).toBeInTheDocument();
   });
+
+  describe('goals and habits', () => {
+    it.todo('should allow a user to save a default set of habits');
+    it.todo(
+      'should display a list of habits if there are any that have been saved'
+    );
+    it.todo('should allow a user to mark a habit as complete today');
+    it.todo('should allow a user to mark a complete habit as not complete');
+    it.todo('should allow a user to delete a habit');
+    it.todo('should allow a user to switch to a weekly view of habits');
+  });
 });
